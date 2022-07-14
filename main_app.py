5d67eb8aeec26e175e159b88bfc34423000a3cce
pajzl-42 <99677624+pajzl-42@users.noreply.github.com>
2022-07-14 19:15:58 +0200

    Add files via upload
    Second

--- main_app.py
+++ main_app.py
@@ -2,3 +2,11 @@ import streamlit as st
 
 st.title("Moje první appka")
 st.write("Toto je moje první aplikace, další řádky budou super cool")
+
+page = st.sidebar.radio("Select page", ["Test", "Thomson"])
+
+if page == "Test":
+	st.write("Toto je moje první aplikace, další řádky budou super cool")
+	
+if page == "Thomson":
+	st.write("Toto je Thomson")
