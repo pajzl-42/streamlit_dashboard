b69c27ab948051161ae48c2cc99c50989cdcd585
pajzl-42 <99677624+pajzl-42@users.noreply.github.com>
2022-07-14 20:45:36 +0200

    Add files via upload
    slider 2

--- main_app.py
+++ main_app.py
@@ -20,8 +20,8 @@ if page == "Mapa":
 	df_temp = df_bikes[selected_columns]
 	df_temp = df_temp.sample(frac=0.5)
 	
-	from_hour_morning = st.slider("Ráno od: ", min_value = 6, max_value = 12)
-	to_hour_morning = st.slider("Ráno do: ", min_value = 6, max_value = 12)
+	from_hour_morning = col1.slider("Ráno od: ", min_value = 6, max_value = 12, value = 7)
+	to_hour_morning = col1.slider("Ráno do: ", min_value = 6, max_value = 12, valuie = 10)
 	
 	query_morning = """ select 
 		hour(started_at),start_station_latitude as lat, start_station_longitude as lon
@@ -30,10 +30,13 @@ if page == "Mapa":
 	
 	df_bikes_morning = pd.read_sql(sql=query_morning, con=engine)
 	
+	from_hour_afternoon = col1.slider("Večer od: ", min_value = 16, max_value = 22, value = 17)
+	to_hour_afternoon = col1.slider("Večer do: ", min_value = 16, max_value = 23, valuie = 17)
+	
 	query_afternoon = """ select 
 		hour(started_at),start_station_latitude as lat, start_station_longitude as lon
 	from edinburgh_bikes
-	where hour(started_at) between 15 and 19  """
+	where hour(started_at) between {} and {}""".format(from_hour_afternoon,to_hour_Afternoon)
 	
 	df_bikes_afternoon = pd.read_sql(sql=query_afternoon, con=engine)
 
