9535e51d6e83a5a02f2f2fbaad750c16901d0526
pajzl-42 <99677624+pajzl-42@users.noreply.github.com>
2022-07-14 19:25:09 +0200

    Add files via upload
    Import dat?

--- main_app.py
+++ main_app.py
@@ -1,7 +1,13 @@
 import streamlit as st
+from sqlalchemy import create_engine
+import pandas as pd
+import pymysql
+
+engine = create_engine("mysql+pymysql://data-student:u9AB6hWGsNkNcRDm@data.engeto.com:3306/data_academy_04_2022")
+query = 'SELECT * FROM covid19_basic ORDER BY date DESC LIMIT 10'
+df = pd.read_sql(sql=query, con=engine)
 
 st.title("Moje první appka")
-st.write("Toto je moje první aplikace, další řádky budou super cool")
 
 page = st.sidebar.radio("Select page", ["Test", "Thomson"])
 
