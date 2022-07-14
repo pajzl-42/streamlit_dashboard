e509a04f19ffc8e3f1a5aca06464b95dee0d9aa4
pajzl-42 <99677624+pajzl-42@users.noreply.github.com>
2022-07-14 19:29:41 +0200

    Add files via upload
    Data?

--- main_app.py
+++ main_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from sqlalchemy import create_engine
 import pandas as pd
 import pymysql
+import plotly.graph_objects as go
+import plotly.express as px
 
 engine = create_engine("mysql+pymysql://data-student:u9AB6hWGsNkNcRDm@data.engeto.com:3306/data_academy_04_2022")
 query = 'SELECT * FROM covid19_basic ORDER BY date DESC LIMIT 10'
@@ -9,10 +11,13 @@ df = pd.read_sql(sql=query, con=engine)
 
 st.title("Moje první appka")
 
-page = st.sidebar.radio("Select page", ["Test", "Thomson"])
+page = st.sidebar.radio("Select page", ["Mapa", "Thomson"])
 
-if page == "Test":
-	st.write("Toto je moje první aplikace, další řádky budou super cool")
-	
+if page == "Mapa":
+	st.write("Mapa používání sdílených kol v Edinburghu")
+	fig = px.scatter_mapbox(df_temp,lat='start_station_latitude', lon='start_station_longitude')
+	fig.update_layout(mapbox_style="open-street-map")
+	fig.show()
+		
 if page == "Thomson":
 	st.write("Toto je Thomson")
